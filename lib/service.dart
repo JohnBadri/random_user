348877a504edbc5e2cacf5d0aadaee034034ccc7
JohnBadri <135007003+JohnBadri@users.noreply.github.com>
2026-04-15 22:18:06 +1000

    added service request

--- lib/service.dart
+++ lib/service.dart
@@ -1,1 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:random_user/model.dart';
 
+class UserService {
+  Future<User> fetchUser() async {
+    final uri = Uri.https(
+      'randomuser.me',
+      'api',
+    );
+
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to obtain random user data');
+    }
+  }
+}
